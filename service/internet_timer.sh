ec9558a545d2fdb57f7908afcdbe9bde84bfe2bb
Pablo Bustos <pablo.bustos@chainalysis.com>
2023-05-01 21:07:32 -0500

    rename

--- service/internet_timer.sh
+++ service/internet_timer.sh
@@ -2,4 +2,4 @@
 
 # location: /usr/local/bin/internet_timer.sh
 
-python3 /home/pi/git/internet/internet_timer.py >> /tmp/internet_timer.log 2>&1
+python3 /home/pi/git/internet_timer/internet_timer.py >> /tmp/internet_timer.log 2>&1
